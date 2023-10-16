0aea838e1b9b5a4cb44b8db8784a90a319e5a9f4
FreddyPinto <freddypinto@outlook.com>
2023-10-16 17:08:00 -0500

    Crea conexión

--- project/main.py
+++ project/main.py
@@ -1,2 +1,21 @@
+from config import url
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
 if __name__ == '__main__':
-    print("Hola mundo")
+
+    # Crear un nuevo cliente y conectarse al servidor
+    client = MongoClient(url, server_api=ServerApi('1'))
+
+    # Acceder a la base de datos 'Anycompany'
+    db = client['Anycompany']
+
+    # Acceder a la colección 'users'
+    collection = db['users']
+
+    # Enviar un ping para confirmar una conexión exitosa
+    try:
+        client.admin.command('ping')
+        print("Has hecho ping a tu despliegue. ¡Te has conectado exitosamente a MongoDB!")
+    except Exception as e:
+        print(e)
