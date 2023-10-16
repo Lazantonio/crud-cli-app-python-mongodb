75cea88b69130433dcf679d884b6f31814cc5f25
FreddyPinto <freddypinto@outlook.com>
2023-10-16 17:29:27 -0500

    Crea functions.py

---
+++ project/functions.py
@@ -0,0 +1,21 @@
+def create_user():
+    '''A) Crear un usuario'''
+    print('Crear usuario')
+
+
+def get_user():
+    '''B) Consultar un usuario'''
+    print('Obtener usuario')
+
+
+def delete_user():
+    '''C) Eliminar un usuario'''
+    print('Eliminar usuario')
+
+
+def update_user():
+    '''D) Actualizar un usuario'''
+    print('Actualizar usuario')
+
+def default(*args, **kwargs):
+    print('Opción no valida')
