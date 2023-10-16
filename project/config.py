9dd781ae60889b68bb0091225a1fa96ef00e0287
FreddyPinto <freddypinto@outlook.com>
2023-10-16 17:09:38 -0500

    Elimina environment variable de url

--- project/config.py
+++ project/config.py
@@ -1,5 +1,5 @@
 from decouple import config
 
-url = "mongodb+srv://freddypinto:{MONGODB_PASSWORD}@cluster0.woyzvx9.mongodb.net/?retryWrites=true&w=majority".format(
+url = "mongodb+srv://freddypinto:{}@cluster0.woyzvx9.mongodb.net/?retryWrites=true&w=majority".format(
     config('MONGODB_PASSWORD', default='password')
 )
